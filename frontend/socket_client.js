17b3e2d30443e330e00cec29fd02fd9594e66b75
urytururur <torbjorn.ekstedt@hotmail.com>
2023-04-17 02:03:58 +0200

    done

--- frontend/socket_client.js
+++ frontend/socket_client.js
@@ -1,24 +1,152 @@
+//initialization functions
+db = {
+  pages: new Map(),
+  players: new Map(),
+  localPlayer: {
+    username: null,
+    move: null
+  },
+  canvas: {
+    width: 100,
+    height: 100,
+    spaceWidth: 10
+  }
+}
+
+storeAllPages = () => {
+
+  for(let element of document.body.children)
+  {
+    if(element.nodeName == "DIV")
+    {
+      db.pages.set(element.id, element);
+    }
+  }
+}
+
+removeAllPages = () => {
+
+  for(let page of db.pages)
+  {
+    page[1].remove();
+  }
+}
+
+addPage = (elementId) => {
+  page = db.pages.get(elementId)
+  document.body.appendChild(page);
+}
+
+removePage = (elementId) => {
+  let element = document.querySelector("#" + elementId);
+  element.remove();
+}
+//--------
+
+//initialize
+storeAllPages();
+removeAllPages();
+addPage("home_page");
+//--------
 
 //socket receiving endpoints
 var socket = io();
 
-socket.on('test', function(message) {
-  console.log(message)
+socket.on('error', function(data) {
+  alert(data.message)
+});
+
+socket.on('show_page', function(data) {
+  removeAllPages();
+  addPage(data.elementId);
+});
+
+socket.on('hide_start_button', function(data) {
+  let startButton = document.querySelector("#start_game_button");
+  startButton.remove();
+});
+
+socket.on('player_info', function(data) {
+
+  for(let player of data.players)
+  {
+    db.players.set(player.username, player);
+  }
+
+  updateCanvas();
+});
+
+socket.on('start_game', function(data) {
+  window.addEventListener('keydown', sendNextMove, false);
 });
 //--------
 
 //pre functions
-function pre_send()
+function pre_joinGame()
 {
-  let pre_message = document.querySelector("#testinput").value;
-  send(pre_message);
+  let pre_username = document.querySelector("#username").value;
+  joinGame(pre_username);
+}
+
+function pre_startGame()
+{
+  socket.emit('start_game', {})
 }
 //--------
 
-//other
-function send(message) {
-  socket.emit('test', {
-    message: message
+//Other functions
+function joinGame(username) {
+  socket.emit('join_game', {
+    username: username
   });
+
+  db.localPlayer.username = username;
 }
+
+socket.on('player_died', function(data) {
+  alert("\"" + data.username + "\" died!:(")
+});
 //--------
+
+function updateCanvas() {
+  const canvas = document.getElementById("tutorial");
+
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    
+    for(let playerInfo of db.players)
+    {
+      let player = playerInfo[1];
+
+      let redAsHex = player.color.substring(0, 2);
+      let greenAsHex = player.color.substring(2, 4);
+      let blueAsHex = player.color.substring(4, 6);
+
+      let red = parseInt(redAsHex, 16);
+      let green = parseInt(greenAsHex, 16);
+      let blue = parseInt(blueAsHex, 16);
+
+      ctx.fillStyle = "rgba(" + red + ", " + green + ", " + blue + ")";
+      ctx.fillRect(player.pixelCoords.x, player.pixelCoords.y, db.canvas.spaceWidth, db.canvas.spaceWidth);
+    }
+  }
+}
+
+function sendNextMove(e) {
+
+  let keyCode = e.keyCode;
+  let validKeyCodes = [37, 38, 39, 40];
+
+  if(!validKeyCodes.includes(keyCode))
+  {
+    return;
+  }
+
+  let localPlayer = db.localPlayer;
+  localPlayer.move = e.keyCode;
+
+  socket.emit('update_move', {
+    username: localPlayer.username,
+    nextMove: localPlayer.move
+  })
+}
--- server.js
+++ server.js
@@ -7,27 +7,254 @@ const io = new Server(server);
 
 app.use(express.static('frontend'))
 
+let db = {
+  players: new Map(),
+  gameStarted: false,
+  canvas: {
+    width: 120,
+    height: 80,
+    spaceWidth: 10
+  },
+  gameLoop: null,
+  tickMillis: 100,
+  occupiedSpaces: []
+}
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + 'frontend/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  debug("A user connected", 0);
 
-  socket.on('test', (data) => {
-    console.log(data.message);
+  socket.on('join_game', (data) => {
 
-    socket.emit('test', {
-      message: "hello from server!"
-    })
+    if(db.gameStarted)
+    {
+      socket.emit('error', {
+        message: 'The game has already started'
+      });
+    }
+
+    if(db.players.has(data.username))
+    {
+      socket.emit('error', {
+        message: 'The specified username is already in use'
+      });
+
+      debug("Player already exists", 1);
+
+      return;
+    }
+
+    let playerCoords = {
+      x: db.players.size % 2 == 0 ? 0 : db.canvas.width - 1,
+      y: db.players.size % 2 == 0 ? 0 : db.canvas.height - 1
+    }
+
+    let pixelCoords = playerPosToPixelPos(playerCoords.x, playerCoords.y);
+
+    let playerInitialMove = db.players.size % 2 == 0 ? 39 : 37;
+
+    db.players.set(data.username, {
+      username: data.username,
+      color: generateRandomColor(),
+      playerCoords: {
+        x: playerCoords.x,
+        y: playerCoords.y
+      },
+      pixelCoords: {
+        x: pixelCoords.x,
+        y: pixelCoords.y
+      },
+      move: playerInitialMove,
+      dead: false
+    });
+
+
+    db.occupiedSpaces.push(playerCoords);
+
+    debug("A new player has joined the game", 1);
+
+    socket.emit('show_page', {
+      elementId: 'game_page'
+    });
+
+    sendPlayerInfo();
+  });
+
+  socket.on('start_game', (data) => {
+    db.gameStarted = true;
+
+    io.emit('hide_start_button', {});
+
+    startGame();
+  });
+
+  socket.on('update_move', (data) => {
+    let player = db.players.get(data.username);
+    if(data.nextMove == (player.move + 2) ||
+       data.nextMove == (player.move - 2))
+    {
+      return;
+    }
+    player.move = data.nextMove;
+    db.players.set(player.username, player);
   });
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    debug("A user disconnected", 0);
   });
+
+  debug(db, 0);
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  debug('listening on *:3000', 0);
 });
 
+generateRandomColor = () =>
+{
+  return Math.floor(Math.random()*16777215).toString(16);
+}
+
+startGame = () =>
+{
+  io.emit('start_game', {});
+
+  db.gameLoop = setInterval(() => {
+
+    for(let playerInfo of db.players)
+    {
+      let player = playerInfo[1];
+
+      if(player.dead)
+      {
+        return;
+      }
+
+      player.playerCoords = getNextPosition(player.move, player.username);
+      player.pixelCoords = playerPosToPixelPos(player.playerCoords.x, player.playerCoords.y);
+
+      //check for loosing conditions
+      if(player.playerCoords.x < 0 ||
+         player.playerCoords.y < 0 ||
+         player.playerCoords.x >= db.canvas.width ||
+         player.playerCoords.y >= db.canvas.height)
+      {
+        player.dead = true
+      }
+      
+      for(let occupiedCoords of db.occupiedSpaces)
+      {
+        if(player.playerCoords.x == occupiedCoords.x &&
+          player.playerCoords.y == occupiedCoords.y)
+        {
+          player.dead = true; 
+        }
+      }
+
+      db.players.set(player.username, player);
+
+      if(player.dead)
+      {
+        clearInterval(db.gameLoop);
+
+        io.emit('player_died', {
+          username: player.username
+        })
+
+        db = {
+          players: new Map(),
+          gameStarted: false,
+          canvas: {
+            width: 120,
+            height: 80,
+            spaceWidth: 10
+          },
+          gameLoop: null,
+          tickMillis: 100,
+          occupiedSpaces: []
+        }
+
+        return;
+      }
+
+      let playerX = player.playerCoords.x;
+      let playerY = player.playerCoords.y;
+
+      db.occupiedSpaces.push({
+        x: playerX,
+        y: playerY
+      });
+    }
+
+    sendPlayerInfo();
+
+  }, db.tickMillis)
+}
+
+sendPlayerInfo = () => {
+
+  let players = [];
+  
+  for(let player of db.players)
+  {
+    players.push(player[1]);
+  }
+
+  io.emit('player_info', {
+    players: players
+  });
+}
+
+playerPosToPixelPos = (x, y) => {
+  return {
+    x: x * db.canvas.spaceWidth,
+    y: y * db.canvas.spaceWidth
+  }
+}
+
+getNextPosition = (move, username) => {
+
+  let player = db.players.get(username);
+
+  switch(move) {
+    case 37:
+      player.playerCoords.x--;
+      break;
+    case 38:
+      player.playerCoords.y--;
+      break;
+    case 39:
+      player.playerCoords.x++;
+      break;
+    case 40:
+      player.playerCoords.y++;
+      break;
+  }
+
+  return player.playerCoords;
+}
+
+//debug
+debug = (message, channel) => {
+
+  const DEBUG = {
+    enabled: true,
+    channels: []
+  }
+
+  if(DEBUG.enabled)
+  {
+    if(DEBUG.channels.includes(channel))
+    {
+      console.log();
+      console.log("---------------------DEBUG---------------------");
+      console.log(message);
+      console.log("-----------------------------------------------");
+      console.log();
+    }
+  }
+
+}
